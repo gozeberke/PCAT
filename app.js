fadf06c1e367bd6b34fc7c07a4ae6bbcf344340f
gozeberke <73718014+gozeberke@users.noreply.github.com>
2022-01-11 01:32:21 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
+const path =require('path')
+const myLogger=(req,re,next)=>{
+    console.log("Middleware Log 1")
+    next()
+}
+const myLogger2=(req,re,next)=>{
+    console.log("Middleware Log 2")
+    next()
+}
+//Middleware
+app.use(express.static('public'))
+app.use(myLogger)
+app.use(myLogger2)
 app.get('/', (req, res) => {
-    const photo={
-        id:1,
-        name:"Photo Name",
-        description:"Photo Description"
-    }
-  res.send(photo);
+    res.sendFile(path.resolve(__dirname,'temp/index.html'))
 });
 
 const port = 3000;
