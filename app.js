8e2a9b85dc44841ff3975192bad8043d5a169f86
gozeberke <73718014+gozeberke@users.noreply.github.com>
2022-01-11 01:13:31 +0300

    added

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const fn = function () {
-  console.log('deneme');
-};
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    const photo={
+        id:1,
+        name:"Photo Name",
+        description:"Photo Description"
+    }
+  res.send(photo);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı. `);
+});
