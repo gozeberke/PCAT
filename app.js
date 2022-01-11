d13a4b327254833c18e549725484fdba895e365b
gozeberke <73718014+gozeberke@users.noreply.github.com>
2022-01-11 14:43:13 +0300

    Tepmlate Engine-ejs

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
 const express = require('express');
-const app = express();
+const ejs=require('ejs')
 const path =require('path')
+const app = express();
+// Template Engine
+app.set("view engine","ejs") // templat olarak ejs kullanacağız
+
+/*
 const myLogger=(req,res,next)=>{
     console.log("Middleware Log 1")
     next()
 }
 const myLogger2=(req,res,next)=>{
     console.log("Middleware Log 2")
-    next()
+    next()  // bir sonraki middleware ilerlemesi için next methodunu çağırmamız lazım
 }
 //Middleware
+
+app.use(myLogger) // Middleware çalıştırmak için use fonk kullanılır
+app.use(myLogger2)*/
 app.use(express.static('public'))
-app.use(myLogger)
-app.use(myLogger2)
+
+//ROUTES
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'temp/index.html'))
+    //res.sendFile(path.resolve(__dirname,'temp/index.html'))
+    res.render("index")
+});
+app.get('/about', (req, res) => {
+    res.render("about")
+});
+app.get('/add', (req, res) => {
+    //res.sendFile(path.resolve(__dirname,'temp/index.html'))
+    res.render("add")
 });
 
 const port = 3000;
